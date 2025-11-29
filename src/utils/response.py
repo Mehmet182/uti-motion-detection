409017f07b6895afc43f6e895478503dfcbfd972
mehmetaydoğan <127485045+Mehmet182@users.noreply.github.com>
2025-11-29 11:10:02 +0300

    Update response.py

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,14 +1,14 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.MotionDetection.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, MotionDetectionOutputs, MotionDetectionResponse, MotionDetectionExecutor, OutputDetections
+from components.MotionDetection.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, MotionDetectionOutputs, MotionDetectionResponse, MotionDetection, OutputDetections
 
 
 def build_response(context):
     outputDetections = OutputDetections(value=context.motion_detections)
     motionDetectionOutputs = MotionDetectionOutputs(outputDetections=outputDetections)
     motionDetectionResponse = MotionDetectionResponse(outputs=motionDetectionOutputs)
-    motionDetectionExecutor = MotionDetectionExecutor(value=motionDetectionResponse)
-    executor = ConfigExecutor(value=motionDetectionExecutor)
+    motionDetection = MotionDetection(value=motionDetectionResponse)
+    executor = ConfigExecutor(value=motionDetection)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
